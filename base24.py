608ae3220774fff16735c142d0015addf30e3144
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-02-29 14:02:15 +0000

    Add requirements and check for these

--- base24.py
+++ base24.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+
+# First, check modules exist
+from importlib.util import find_spec as module_exists
+#import importlib.util.find_spec as module_exists
+import sys
+if not module_exists("aiofiles") or not module_exists("pystache" or not module_exists("yaml")):
+	print("Missing Modules! Do: pip3 install --upgrade aiofiles pystache PyYAML")
+	sys.exit(1)
+
 import base24_builder
 
 if __name__ == '__main__':
